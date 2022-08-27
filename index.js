d1d8351c0f77505167ad02dd01f22ac09bfa4320
Sthilen <110030158+Sthilen@users.noreply.github.com>
2022-08-27 10:31:20 +0700

    a text file containing links is in the making

--- index.js
+++ index.js
@@ -64,17 +64,20 @@ function secret() {
     return
 }
 
-function randomEvent() {
-    let random = Math.floor(Math.random() * (10 - 0 + 1));
-    switch (random) {
-        case 0:
-            console.log(0)
-            document.getElementById('Random').src='https://cdn.cloudflare.steamstatic.com/steam/apps/1761210/capsule_616x353.jpg?t=1659945847'
-            break;
-        case 1:
-            console.log(1);
-            document.getElementById('Random').src='station.jpg'
-            break;
-    }
-}
+// function randomEvent() {
+//     let random = Math.floor(Math.random() * (10 - 0 + 1));
+//     switch (random) {
+//         case 0:
+//             console.log(0)
+//             document.getElementById('Random').src='https://cdn.cloudflare.steamstatic.com/steam/apps/1761210/capsule_616x353.jpg?t=1659945847'
+//             break;
+//         case 1:
+//             console.log(1);
+//             document.getElementById('Random').src='station.jpg'
+//             break;
+//     }
+// }
 
+function randomEvent() {
+    const doNotClick = readFileSync(links/do_not_click.txt, "utf-8").split('\n')
+}
